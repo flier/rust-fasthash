7953c4bd6e4ef747de365f0caf8749b397e9e315
Flier Lu <flier.lu@gmail.com>
2016-12-26 20:04:07 +0800

    add v0.2.0 documents

---
+++ docs/v0.2.0/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fasthash_sys"] = ["impl&lt;_T1:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>,&nbsp;_T2:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='fasthash_sys/struct.std_namespace_pair.html' title='fasthash_sys::std_namespace_pair'>std_namespace_pair</a>&lt;_T1,&nbsp;_T2&gt;","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='fasthash_sys/struct.uint128_c_t.html' title='fasthash_sys::uint128_c_t'>uint128_c_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='fasthash_sys/enum._bindgen_ty_7.html' title='fasthash_sys::_bindgen_ty_7'>_bindgen_ty_7</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='fasthash_sys/struct._bindgen_ty_8.html' title='fasthash_sys::_bindgen_ty_8'>_bindgen_ty_8</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='fasthash_sys/struct._bindgen_ty_9.html' title='fasthash_sys::_bindgen_ty_9'>_bindgen_ty_9</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/v0.2.0/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fasthash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='fasthash/spooky/struct.SpookyHasher128.html' title='fasthash::spooky::SpookyHasher128'>SpookyHasher128</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='fasthash/xx/struct.XXHasher32.html' title='fasthash::xx::XXHasher32'>XXHasher32</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='fasthash/xx/struct.XXHasher64.html' title='fasthash::xx::XXHasher64'>XXHasher64</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
