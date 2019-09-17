3260ceaf61c2e345f8a12df1b0a15e29a9aef8fe
Flier Lu <flier.lu@gmail.com>
2019-09-17 10:55:34 +0800

    use different binding files for linux and macos

--- fasthash-sys/src/lib.rs
+++ fasthash-sys/src/lib.rs
@@ -1,13 +1,13 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
 use cfg_if::cfg_if;
 
 cfg_if! {
     if #[cfg(feature = "gen")] {
         include!(concat!(env!("OUT_DIR"), "/fasthash.rs"));
-    } else {
-        include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/fasthash.rs"));
+    } else if #[cfg(target_os = "linux")] {
+        include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/linux.rs"));
+    } else if #[cfg(target_os = "macos")] {
+        include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/macos.rs"));
     }
 }
